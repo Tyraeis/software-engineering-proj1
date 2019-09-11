7bc36ea0fa51afbb643a02c72ccbf454c60df15b
Noah Craig <ncraig@ou.edu>
2019-09-11 16:04:39 -0500

    Fix java

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,15 +1,19 @@
-import java.io.IOException;
+import java.util.Random;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import static spark.Spark.*;
 
-@WebServlet(name = "SoftwareEngineeringProject1", value="/")
-public class Main extends HttpServlet {
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/plain");
-        response.getWriter().println("Hello, world!");
+public class Main {
+    private static int RANDOM_MIN = 1;
+    private static int RANDOM_MAX = 1000000;
+
+    private static Random rng = new Random();
+
+    private static int getRandomNumber() {
+        return RANDOM_MIN + rng.nextInt(RANDOM_MAX - RANDOM_MIN + 1);
+    }
+
+    public static void main(String[] argv) {
+        port(8081);
+        get("/", (req, res) -> Integer.toString(getRandomNumber()));
     }
 }
